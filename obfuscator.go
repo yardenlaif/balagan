39b47cbb273a4fd3ce50b6aec602bc37b92e8764
Yarden Laifenfeld <ylaifenfeld@gmail.com>
2024-07-05 18:10:44 +0300

    Support simple obfuscation recursively

--- obfuscator.go
+++ obfuscator.go
@@ -3,11 +3,10 @@ package main
 import (
 	"fmt"
 	"go/ast"
-	"go/importer"
 	"go/printer"
 	"go/token"
 	"go/types"
-	"log"
+	"math/rand"
 	"os"
 	"path"
 	"path/filepath"
@@ -15,6 +14,19 @@ import (
 
 	"golang.org/x/exp/maps"
 )
+
+var exclude = map[string]struct{}{"main": {}, "init": {}, "_": {}}
+
+func getAllImports(pkg *types.Package, imports map[*types.Package]struct{}) {
+	if _, ok := imports[pkg]; ok {
+		return
+	}
+	imports[pkg] = struct{}{}
+
+	for _, imp := range pkg.Imports() {
+		getAllImports(imp, imports)
+	}
+}
 
 func getPublicInterfaces(pkg *types.Package) map[*types.Interface]struct{} {
 	interfaces := make(map[*types.Interface]struct{})
@@ -29,52 +41,93 @@ func getPublicInterfaces(pkg *types.Package) map[*types.Interface]struct{} {
 		}
 		if i, ok := t.(*types.Interface); ok {
 			interfaces[i] = struct{}{}
-			log.Printf("Found interface: %+v\n", i)
 		}
 	}
 	return interfaces
 }
 
-func getPublicInterfacesRecursively(pkg *types.Package) map[*types.Interface]struct{} {
-	interfaces := getPublicInterfaces(pkg)
-	for _, imp := range pkg.Imports() {
-		maps.Copy(interfaces, getPublicInterfacesRecursively(imp))
-	}
-	return interfaces
-}
-
-func findInterfaces(input string, astPkgs []*ast.Package, fset *token.FileSet, info *types.Info) map[*types.Interface]struct{} {
+func getAllPublicInterfaces(pkgs []*types.Package) map[*types.Interface]struct{} {
 	interfaces := make(map[*types.Interface]struct{})
-	conf := &types.Config{Importer: importer.Default()}
-	for _, astPkg := range astPkgs {
-		// Get all interfaces so we don't mess up a type that should satisfy one of these interfaces
-		pkg, _ := conf.Check(input, fset, maps.Values(astPkg.Files), info)
-		maps.Copy(interfaces, getPublicInterfacesRecursively(pkg))
+	for _, pkg := range pkgs {
+		maps.Copy(interfaces, getPublicInterfaces(pkg))
 	}
 	return interfaces
 }
 
-func createObfuscatedNames(info *types.Info, interfaces map[*types.Interface]struct{}) map[types.Object]string {
-	newNames := make(map[types.Object]string)
+func findInterfaces(input string, typesPkgs []*types.Package, fset *token.FileSet, info *types.Info) map[*types.Interface]struct{} {
+	imports := make(map[*types.Package]struct{})
+	for _, pkg := range typesPkgs {
+		// Get all interfaces so we don't mess up a type that should satisfy one of these interfaces
+		getAllImports(pkg, imports)
+	}
+	return getAllPublicInterfaces(maps.Keys(imports))
+}
+
+func funcImplementsInterface(f *types.Func, interfaces map[*types.Interface]struct{}) bool {
+	signature, ok := f.Type().(*types.Signature)
+	if !ok {
+		return false
+	}
+	if signature.Recv() == nil {
+		return false
+	}
+
+	recvType := signature.Recv().Type()
+	for i := range interfaces {
+		if types.Implements(recvType, i) {
+			return true
+		}
+	}
+	return false
+}
+
+func createObfuscatedNames(info *types.Info, interfaces map[*types.Interface]struct{}) map[string]string {
+	newNames := make(map[string]string)
 	// Create obfuscated names for all identifiers in file
 	// TODO: Check type switch statement
 	for ident, obj := range info.Defs {
-		if ident.Name == "main" {
+		if _, ok := exclude[ident.Name]; ok {
 			continue
 		}
-		newNames[obj] = nextName(ident.Name, true)
+		if f, ok := obj.(*types.Func); ok && funcImplementsInterface(f, interfaces) {
+			continue
+		}
+		if obj == nil || obj.Pkg() == nil {
+			continue
+		}
+		newNames[fullName(obj)] = nextName(ident.Name, true)
 	}
 	return newNames
 }
 
-func obfuscateAST(astPkgs []*ast.Package, info *types.Info, obfuscatedNames map[types.Object]string) {
+func fullName(obj types.Object) string {
+	if !obj.Exported() {
+		return obj.Id()
+	} else {
+		return obj.Pkg().Name() + "." + obj.Id()
+	}
+}
+
+func obfuscateAST(astPkgs []*ast.Package, info *types.Info, obfuscatedNames map[string]string) {
 	for ident, obj := range info.Defs {
-		if newName, ok := obfuscatedNames[obj]; ok {
+		if obj == nil || obj.Pkg() == nil {
+			continue
+		}
+		if _, ok := obj.(*types.PkgName); ok {
+			continue
+		}
+		if newName, ok := obfuscatedNames[fullName(obj)]; ok {
 			ident.Name = newName
 		}
 	}
 	for ident, obj := range info.Uses {
-		if newName, ok := obfuscatedNames[obj]; ok {
+		if obj == nil || obj.Pkg() == nil {
+			continue
+		}
+		if _, ok := obj.(*types.PkgName); ok {
+			continue
+		}
+		if newName, ok := obfuscatedNames[fullName(obj)]; ok {
 			ident.Name = newName
 		}
 	}
@@ -106,26 +159,13 @@ func writeAST(input string, output string, astPkgs []*ast.Package, fset *token.F
 	}
 }
 
-func obfuscate(input string, fset *token.FileSet, astPkgs []*ast.Package) map[*types.Interface]struct{} {
-	info := types.Info{
-		Scopes: make(map[ast.Node]*types.Scope),
-		Defs:   make(map[*ast.Ident]types.Object),
-		Uses:   make(map[*ast.Ident]types.Object),
-	}
-
-	interfaces := findInterfaces(input, astPkgs, fset, &info)
-	obfuscatedNames := createObfuscatedNames(&info, interfaces)
-	obfuscateAST(astPkgs, &info, obfuscatedNames)
-	writeAST(input, "./output", astPkgs, fset)
-
-	for _, astPkg := range astPkgs {
-		for _, file := range astPkg.Files {
-			printer.Fprint(os.Stdout, fset, file)
-		}
-	}
-	return interfaces
+func obfuscate(input string, output string, fset *token.FileSet, astPkgs []*ast.Package, typesPkgs []*types.Package, info *types.Info) {
+	interfaces := findInterfaces(input, typesPkgs, fset, info)
+	obfuscatedNames := createObfuscatedNames(info, interfaces)
+	obfuscateAST(astPkgs, info, obfuscatedNames)
+	writeAST(input, output, astPkgs, fset)
 }
 
 func nextName(currentName string, isExported bool) string {
-	return currentName + "1"
+	return currentName + string(rune(rand.Intn(int('z'-'a'))+'a'))
 }
